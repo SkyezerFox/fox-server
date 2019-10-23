49de627cbc5d36b4ebcac205b363475b3595ed79
sparklfox <actuallyori@gmail.com>
2019-10-23 14:00:22 +0100

    attempt to fix typing not being detected

--- tests/index.js
+++
@@ -1,7 +0,0 @@
-const fox = require("../dist");
-
-const server = new fox.FoxServer();
-
-server.task(() => new Promise((r) => setTimeout(r, 1e3)));
-
-server.start();
---
+++ tests/index.ts
@@ -0,0 +1,7 @@
+import { FoxServer } from "../dist";
+
+const server = new FoxServer();
+
+server.task(() => new Promise((r) => setTimeout(r, 1e3)));
+
+server.start();
